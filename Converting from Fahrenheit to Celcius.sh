08dfe2ca0455e9d9e398468ef4b2826554794ea5
Alex Dzewaltowski <towski@ksu.edu>
2016-09-09 08:52:53 -0500

    I've made the file conversion also output kelvin

--- Converting from Fahrenheit to Celcius.sh
+++ Converting from Fahrenheit to Celcius.sh
@@ -4,4 +4,8 @@ read Fahrenheit
 
 Celcius=$(echo "scale=2;(5/9)*($Fahrenheit-32)"|bc)
 
+Kelvin=$(echo "scale=2;$Celcius+273.2"|bc)
+
 echo "$Fahrenheit Fahrenheit is $Celcius Celcius"
+
+echo "$Fahrenheit Fahrenheit is $Kelvin Kelvin"
